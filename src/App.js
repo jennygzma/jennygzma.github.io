d27519aa80b08db9a1b169888beeb5eae91a3ce5
jenny ma <jennyma@jennys-mbp-2.lan>
2022-12-04 15:25:41 -0800

    background color

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar/>
-      <About/>
-      <Experiences/>
-      <News/>
-      <Blog/>
+      <div className='bg-cream'> 
+        <Navbar/>
+        <About/>
+        <Experiences/>
+        <News/>
+        <Blog/>
+      </div>
     </main>
   );
 }
