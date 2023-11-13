34145d75cb3d0a3583a4beb90f3ee9838dbb1ee4
jenny ma <jennyma@jennys-mbp-2.mynetworksettings.com>
2023-11-13 08:44:16 -0500

    upadte portfolio icon to zoro

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import React from 'react';
-import About from './components/About'
-import Experiences from './components/Experiences'
-import News from './components/News'
-import Blog from './components/Blog'
-import Navbar from './components/Navbar'
+import "./App.css";
+import React from "react";
+import About from "./components/About";
+import Experiences from "./components/Experiences";
+import News from "./components/News";
+import Blog from "./components/Blog";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font-serif bg-cream">
-      <div className='mx-1'> 
-        <Navbar/>
-        <About/>
-        <Experiences/>
-        <News/>
-        <Blog/>
+      <div className="mx-1">
+        <Navbar />
+        <About />
+        <Experiences />
+        <News />
       </div>
     </main>
   );
